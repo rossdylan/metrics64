dacf147605e7384594f90ee129cb898901fcc7e0
Ross Delinger <rossdylan@fastmail.com>
2024-06-09 13:45:12 -0700

    [metrics64] Move M::must call into registry

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use gxhash::HashMap;
 use parking_lot::RwLock;
 use std::any::Any;
+use std::collections::hash_map::Entry;
 use std::{
     hash::{Hash, Hasher},
     marker::PhantomData,
@@ -17,19 +18,26 @@ use std::{
 
 /// An enum that defines a set of global location tags that are evalulated once per process
 /// to provide location/targetting information.
+#[derive(Debug, Copy, Clone)]
 pub enum Target {
     /// Emit per-pod related location information
     Pod,
 }
 
+/// A trait representing the public metric interface. This is common across all metric kinds, and slots into the
+/// general [`MetricDefinition`] to provide the backing implementation.
 pub trait Metric: Sized {
-    fn must() -> Self;
+    fn must(mid: u64) -> Self;
 }
 
+/// A trait representing the internal chunk of the metric interface. We use this to collect observations of the
+/// underlying metric value, as well as store references to the metric in the registry.
 pub trait Recordable: Send + Sync + 'static {
     fn as_any(&self) -> &dyn Any;
 }
 
+/// A constant definition of a metric. Provides a single spot for defining the schema of a metric at compile time
+/// Internally it will handle calling out to the registry at runtime to register the new metric.
 pub struct MetricDef<M> {
     name: &'static str,
     tags: &'static [&'static str],
@@ -50,14 +58,15 @@ where
         }
     }
 
+    /// A note about the ordering here.
+    /// I think we can push most of the underyling metric storage/allocation down into the registry. So instead
+    /// of the current back and forth with mid, get, register we just call `create` or something and internally
+    /// the registry sets up our singular storage location for metadata and the metric itself and just returns
+    /// the core metric. This should allow us to avoid having to spread the allocations for metric metadata
+    /// into each of the individual metrics and instead we can keep them all together in the hashmap. or
+    /// maybe we use a slab/pool since its all pretty small.
     pub fn must(&self, tags: &[(&'static str, &'static str)]) -> M {
-        let mid = DEFAULT_REGISTRY.mid(self.name, tags);
-        if let Some(m) = DEFAULT_REGISTRY.get(mid) {
-            return m;
-        }
-        let m = M::must();
-        DEFAULT_REGISTRY.register(mid, m.clone());
-        m
+        DEFAULT_REGISTRY.register(self.name, self.target, tags)
     }
 }
 
@@ -78,7 +87,7 @@ impl Counter {
 }
 
 impl Metric for Counter {
-    fn must() -> Self {
+    fn must(_mid: u64) -> Self {
         Self {
             inner: Default::default(),
         }
@@ -91,9 +100,32 @@ impl Recordable for Counter {
     }
 }
 
+struct MetricMetadata {
+    name: &'static str,
+    target: Target,
+    tags: Vec<(&'static str, &'static str)>,
+    metric: Box<dyn Recordable>,
+}
+
+impl MetricMetadata {
+    pub fn new<R: Recordable>(
+        name: &'static str,
+        target: Target,
+        tags: &[(&'static str, &'static str)],
+        metric: R,
+    ) -> Self {
+        Self {
+            name,
+            target,
+            tags: Vec::from(tags),
+            metric: Box::new(metric),
+        }
+    }
+}
+
 #[derive(Default)]
 pub struct Registry {
-    metrics: RwLock<HashMap<u64, Box<dyn Recordable>>>,
+    metrics: RwLock<HashMap<u64, MetricMetadata>>,
 }
 
 static DEFAULT_REGISTRY: LazyLock<Registry> = LazyLock::new(Registry::new);
@@ -107,27 +139,35 @@ impl Registry {
 
     /// Calculate a metric-id for this metric. This is used as a key internally to the registry to handle lookups of
     /// registered metrics.
-    pub fn mid(&self, name: &'static str, tags: &[(&'static str, &'static str)]) -> u64 {
+    fn mid(&self, name: &'static str, tags: &[(&'static str, &'static str)]) -> u64 {
         let mut hasher = gxhash::GxHasher::with_seed(MID_SEED);
         name.hash(&mut hasher);
         tags.hash(&mut hasher);
         hasher.finish()
     }
 
-    /// Attempt to get the metric registered under the givee mid. We do some downcasting magic to return
-    /// the underlying metric if it exists. This ensures that if somehow we attempt to create the same
-    /// exact metric twice they will share the underlying metric implementation.
-    pub fn get<R: Recordable + Clone>(&self, mid: u64) -> Option<R> {
-        let metrics = self.metrics.read();
-        metrics
-            .get(&mid)
-            .and_then(|m| m.as_any().downcast_ref::<R>())
-            .cloned()
-    }
-
-    pub fn register<R: Recordable>(&self, mid: u64, metric: R) {
-        if self.metrics.write().insert(mid, Box::new(metric)).is_some() {
-            panic!("attempted to double registry metric {mid}");
+    pub fn register<R: Recordable + Metric + Clone>(
+        &self,
+        name: &'static str,
+        target: Target,
+        tags: &[(&'static str, &'static str)],
+    ) -> R {
+        let mid = self.mid(name, tags);
+        let mut metrics = self.metrics.write();
+        let entry = metrics.entry(mid);
+        match entry {
+            Entry::Occupied(dr) => dr
+                .get()
+                .metric
+                .as_any()
+                .downcast_ref::<R>()
+                .cloned()
+                .expect("attempted to register metric with same mid with different type"),
+            Entry::Vacant(ve) => {
+                let metric = R::must(mid);
+                ve.insert(MetricMetadata::new(name, target, tags, metric.clone()));
+                metric
+            }
         }
     }
 }
