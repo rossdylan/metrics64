4f85dcf0f3a3dae85e30a92821a6a5506c4b53a3
Ross Delinger <rossdylan@fastmail.com>
2025-03-08 09:12:29 -0800

    Add a pure rust impl of ldexp
    I decided to reimplement ldexp and frexp in rust by porting the underlying libc
    implementations. I've chosen to base them on musl libc for now. This should have
    the same performance characteristics, but avoid any extra unsafe or linking
    shenanigans.

--- src/cmath.rs
+++ src/cmath.rs
@@ -17,9 +17,60 @@ mod ffi {
     }
 }
 
+/// A naive rust copy of the musl libc ldexp function
 pub fn ldexp(frac: f64, exp: isize) -> f64 {
-    unsafe { ffi::ldexp(frac, exp as c_int) }
+    let mut y = frac;
+    let mut n = exp;
+
+    if n > 1023 {
+        y *= 2_f64.powi(1023);
+        n -= 1023;
+        if n > 1023 {
+            y *= 2_f64.powi(1023);
+            n -= 1023;
+            if n > 1023 {
+                n = 1023;
+            }
+        }
+    } else if n < -1022 {
+        /* make sure final n < -53 to avoid double
+        rounding in the subnormal range */
+        y *= 2_f64.powi(-1022) * 2_f64.powi(53);
+        n += 1022 - 53;
+        if n < -1022 {
+            y *= 2_f64.powi(-1022) * 2_f64.powi(53);
+            n += 1022 - 53;
+            if n < -1022 {
+                n = -1022;
+            }
+        }
+    }
+    let int: u64 = ((0x3ff + n) as u64) << 52;
+    let float: f64 = f64::from_bits(int);
+    let res = y * float;
+    debug_assert_eq!(res, unsafe { ffi::ldexp(frac, exp as c_int) });
+    res
 }
+
+//fn frexp_inner(value: f64, out: &mut isize) {
+//    let int = value.to_bits() >> 52 & 07ff;
+//	if (!int) {
+//		if (value) {
+//			 = frexp_inner(x* 2_f64.powi(64), out);
+//			*e -= 64;
+//		} else {
+//		  *e = 0;
+//		}
+//		return x;
+//	} else if (ee == 0x7ff) {
+//		return x;
+//	}
+//
+//	*e = ee - 0x3fe;
+//	y.i &= 0x800fffffffffffffull;
+//	y.i |= 0x3fe0000000000000ull;
+//	return y.d;
+//}
 
 pub fn frexp(value: f64) -> (f64, isize) {
     unsafe {
